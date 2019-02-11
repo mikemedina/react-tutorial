4adfa82510df7ec071352db8121d9b2c6ba55c1e
Mike Medina <mikemedina805@gmail.com>
2019-02-10 20:29:48 -0500

    Add an initial component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log("It works!?");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
